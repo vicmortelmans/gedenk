03bc78ce39d0f0cec93a2681d53167fdd07565d7
Vic Mortelmans <vic.mortelmans@telenet.be>
2014-02-13 14:16:50 -0500

    adjusting javascript to collapse subtitle

--- gedenk.js
+++ gedenk.js
@@ -1,11 +1,11 @@
 var Webflow = Webflow || [];  
 Webflow.push(function () { 
     $('h1').on('click', function() {
-        if ( $(this).parent().find('.slider').is(':visible') ) {
-            $('.slider').slideUp();
+        if ( $(this).parent().find('.content').is(':visible') ) {
+            $('.content').slideUp();
         } else {
-            $('.slider').hide();
-            $(this).parent().find('.slider').slideDown();
+            $('.content').hide();
+            $(this).parent().find('.content').slideDown();
             $('html, body').animate({
                 scrollTop: ($(this).offset().top)
             },500);
